2bd685b7c9d678b26b496d41f8faf7141b0260b4
Jordan McGuire <jordandmcguire@gmail.com>
2022-03-10 14:25:00 -0700

    Updates to Schedule Class
    Created fields and methods for Schedule Class

--- src/main/java/JavaConf2020.java
+++ src/main/java/JavaConf2020.java
@@ -1,9 +1,60 @@
-
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Stack;
 
 public class JavaConf2020 {
 
     public static void main(String[] args) {
+
+        HashMap<Integer, Stack<String>> sessions = new HashMap<>();
+        ArrayList<String> scheduleList = new ArrayList<>();
+        Schedule testSchedule = new Schedule(195, sessions, scheduleList);
+
+        int key45 = 45;
+        int key30 = 30;
+
+        String session1 = "Java and Containers - Make it Awesome! - 45 min";
+        String session2 = "Memory Efficient Java - 45 min";
+        String session3 = "Discover Modern Java - 45 min";
+
+        String session4 = "Java: Did you Know That? - 30 min";
+        String session5 = "Property-Based Testing in Java - 30 min";
+
+        testSchedule.addSession(key45, session1);
+        testSchedule.addSession(key45, session2);
+        testSchedule.addSession(key45, session3);
+        testSchedule.addSession(key30, session4);
+        testSchedule.addSession(key30, session5);
+
+        // state 1
+        System.out.println("State 1: ");
+        System.out.println("Total Sessions Available: " + testSchedule.getSessions());
+        System.out.println("Schedule: " + testSchedule.getScheduleList());
+
+        testSchedule.addToTrack1(key30);
+
+        // state 2
+        System.out.println();
+        System.out.println("State 2: ");
+        System.out.println("Total Sessions Available: " + testSchedule.getSessions());
+        System.out.println("Schedule: " + testSchedule.getScheduleList());
+
+        testSchedule.addToTrack1(key30);
+
+        // state 3
+        System.out.println();
+        System.out.println("State 3: ");
+        System.out.println("Total Sessions Available: " + testSchedule.getSessions());
+        System.out.println("Schedule: " + testSchedule.getScheduleList());
+
+        testSchedule.addToTrack1(key45);
+        testSchedule.addToTrack1(key45);
+
+        // state 4
+        System.out.println();
+        System.out.println("State 4: ");
+        System.out.println("Total Sessions Available: " + testSchedule.getSessions());
+        System.out.println("Schedule: " + testSchedule.getScheduleList());
 
     }
 }
--- src/main/java/Schedule.java
+++ src/main/java/Schedule.java
@@ -1,6 +1,134 @@
-
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Stack;
 
 public class Schedule {
+
+    // These ints will hold the total number of minutes available in each track
+    private int track1MinutesAvailable;
+    private int track2MinutesAvailable;
+    /*
+    This hashmap is for storing the sessions. Because hashmap have unique keys,
+    The key would be the number of minutes in the session. The Stacks will
+    hold the string representations of each session with the corresponding number
+    of minutes. So if 3 sessions are each 45 minutes long, then the key 45 will
+    contain a stack of 3 strings. When the string has been added to the scheduleList,
+    the stack can be popped to make way for the next session.
+    */
+    private HashMap<Integer, Stack<String>> sessions = new HashMap<>();
+    private ArrayList<String> scheduleList = new ArrayList<>();
+
+    // Empty constructor
+    public Schedule() {
+    }
+
+    // Fully Parameterized constructor
+    public Schedule(int track1MinutesAvailable, int track2MinutesAvailable,
+                    HashMap<Integer, Stack<String>> sessions, ArrayList<String> scheduleList) {
+
+        this.track1MinutesAvailable = track1MinutesAvailable;
+        this.track2MinutesAvailable = track2MinutesAvailable;
+        this.sessions = sessions;
+        this.scheduleList = scheduleList;
+
+    }
+
+    // Constructor with only 1 track for testing purposes
+    public Schedule(int track1MinutesAvailable, HashMap<Integer, Stack<String>> sessions,
+                    ArrayList<String> scheduleList) {
+
+        this.track1MinutesAvailable = track1MinutesAvailable;
+        this.sessions = sessions;
+        this.scheduleList = scheduleList;
+
+    }
+
+    public int getTrack1MinutesAvailable() {
+
+        return track1MinutesAvailable;
+
+    }
+
+    public void setTrack1MinutesAvailable(int track1MinutesAvailable) {
+
+        this.track1MinutesAvailable = track1MinutesAvailable;
+
+    }
+
+    public int getTrack2MinutesAvailable() {
+
+        return track2MinutesAvailable;
+
+    }
+
+    public void setTrack2MinutesAvailable(int track2MinutesAvailable) {
+
+        this.track2MinutesAvailable = track2MinutesAvailable;
+
+    }
+
+    public HashMap<Integer, Stack<String>> getSessions() {
+
+        return sessions;
+
+    }
+
+    public void setSessions(HashMap<Integer, Stack<String>> sessions) {
+
+        this.sessions = sessions;
+
+    }
+
+    public void addSession(int key, String session) {
+
+        if (sessions.containsKey(key)) {
+            Stack<String> plusOne = sessions.get(key);
+            plusOne.push(session);
+            sessions.put(key, plusOne);
+        }
+        else {
+            Stack<String> plusOne = new Stack<>();
+            plusOne.push(session);
+            sessions.put(key, plusOne);
+        }
+
+    }
+
+    public void removeSession(int key, String session) {
+
+        if (sessions.containsKey(key)) {
+            Stack<String> plusOne = sessions.get(key);
+            plusOne.remove(session);
+            sessions.put(key, plusOne);
+        }
+
+    }
+
+    public ArrayList<String> getScheduleList() {
+
+        return scheduleList;
+
+    }
+
+    public void setScheduleList(ArrayList<String> scheduleList) {
+
+        this.scheduleList = scheduleList;
+
+    }
+
+    public void addToTrack1(int minutes) {
+
+        if (track1MinutesAvailable - minutes < 0) {
+            System.out.println("That track does not " +
+                    "have enough time left. Required: " + (track1MinutesAvailable - minutes) +
+                    " Available: " + (track1MinutesAvailable));
+        }
+        else {
+            track1MinutesAvailable = track1MinutesAvailable - minutes;
+            scheduleList.add(sessions.get(minutes).peek());
+            sessions.get(minutes).pop();
+        }
+
+    }
 
 }
